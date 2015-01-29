8346e5a7498aa4ef2e79294f2320622ba75821c0
Isagani Mendoza <gani.mendoza@gmail.com>
2015-01-29 19:44:59 +0800

    Added RUNFLAG

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 //License: MIT
-//Gani Mendoza (itjumpstart.wordpress.com)
+//Author: Gani Mendoza (itjumpstart.wordpress.com)
 
 package main
 
 import (
 	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
-	"github.com/fatih/color"
+	"github.com/daviddengcn/go-colortext"
 	anko_core "github.com/mattn/anko/builtins"
 	anko_encoding "github.com/mattn/anko/builtins/encoding"
 	anko_flag "github.com/mattn/anko/builtins/flag"
@@ -22,6 +23,7 @@ import (
 	anko_term "github.com/mattn/anko/builtins/term"
 	"github.com/mattn/anko/parser"
 	"github.com/mattn/anko/vm"
+	"github.com/vaughan0/go-ini"
 	"log"
 	"os"
 	"os/exec"
@@ -29,12 +31,16 @@ import (
 	"strings"
 )
 
+var (
+	cmdfile ini.File
+)
+
 func printError(err error) {
 
 	if err != nil {
-		color.Set(color.FgRed)
+		ct.ChangeColor(ct.Red, false, ct.None, false)
 		os.Stderr.WriteString(fmt.Sprintf("==> ERROR: %s\n", err.Error()))
-		color.Unset()
+		ct.ResetColor()
 	}
 
 }
@@ -42,9 +48,9 @@ func printError(err error) {
 func printOutput(outs []byte) {
 
 	if len(outs) > 0 {
-		color.Set(color.FgGreen)
+		ct.ChangeColor(ct.Green, false, ct.None, false)
 		fmt.Printf("==> OUTPUT: %s\n", string(outs))
-		color.Unset()
+		ct.ResetColor()
 	}
 }
 
@@ -83,6 +89,112 @@ func runCmd(args string) error {
 		printError(err)
 		printOutput(output)
 	}
+	return err
+}
+
+func runflagCmd(args string) error {
+
+	/*
+		- Flag sections must only include key/value entries
+		- To simplify parsing, flag must be either single dash or double dashes
+		- As a consequence of above, do not mix single/double colon variables
+		- E.g. docker -d :singledash ::doubledash (wrong)
+
+		https://docs.docker.com/reference/commandline/cli/
+
+		Case 1: single dash flags
+
+		docker run -d -m 100m -e DEVELOPMENT=1 \
+		-e BRANCH=example-code \
+		-v $(pwd):/app/bin:ro \
+		--name app appserver
+
+		can be rewritten as
+
+		[docker]
+
+		m = 100m
+		e = DEVELOPMENT=1
+		e = BRANCH=example-code
+		v = $(pwd):/app/bin:ro
+
+		[code]
+
+		RUNFLAG sudo docker run -d --name app appserver :docker
+
+
+		Case 2: double dash flags
+
+		[network]
+		nic1 = bridged
+		nic2 = hostonly
+
+		[code]
+
+		RUNFLAG VBoxManage modifyvm tklinux ::network
+
+	*/
+
+	fmt.Println(args)
+
+	var shell, flag string
+	if runtime.GOOS == "windows" {
+		shell = "cmd"
+		flag = "/C"
+	} else {
+		shell = "/bin/sh"
+		flag = "-c"
+	}
+
+	if strings.Contains(args, ":") && strings.Contains(args, "::") {
+		return errors.New("RUNFLAG cannot contain single and double colon variables")
+	}
+
+	//address case 1
+	if strings.Contains(args, ":") {
+		slcArgs := strings.Split(args, ":")
+
+		args1 := slcArgs[0]
+		section := slcArgs[1]
+
+		var flags string
+
+		for key, value := range cmdfile[section] {
+			flags = flags + " -" + key + " " + value
+		}
+
+		fmt.Println(args1 + flags)
+
+		args = args1 + flags
+	}
+
+	//address case 2
+	if strings.Contains(args, "::") {
+		slcArgs := strings.Split(args, "::")
+
+		args1 := slcArgs[0]
+		section := slcArgs[1]
+
+		var flags string
+
+		for key, value := range cmdfile[section] {
+			flags = flags + " --" + key + " " + value
+		}
+
+		fmt.Println(args1 + flags)
+
+		// Output:
+		// VBoxManage modifyvm tklinux --nic1 bridged --nic2 hostonly
+
+		args = args1 + flags
+	}
+
+	cmd := exec.Command(shell, flag, args)
+
+	output, err := cmd.CombinedOutput()
+	printError(err)
+	printOutput(output)
+
 	return err
 }
 
@@ -315,34 +427,58 @@ func processCmd(command string) error {
 		ankoFilename := argCommand
 		err = ankoCmd(ankoFilename)
 
+	case "RUNFLAG":
+		err = runflagCmd(strings.Join(slcStr[1:], " "))
+
 	}
 
 	return err
 }
 
-func main() {
-
-	if len(os.Args) != 2 {
-		printError(errors.New("Please specify a cmdfile"))
-		os.Exit(1)
-	}
-
-	file, err := os.Open(os.Args[1])
-	defer file.Close()
+func parseIniSections(filename string) string {
+	file, err := os.Open(filename)
 
 	if err != nil {
 		printError(err)
 		log.Fatal(err)
 	}
 
-	var ln string
+	var line string
 	scanner := bufio.NewScanner(file)
+
+	hasReachedCodeSection := false
+	strForIniParsing := ""
+
 	for scanner.Scan() {
 
-		ln = scanner.Text()
+		line = scanner.Text()
 
-		if !strings.Contains(ln, "#") {
-			err = processCmd(ln)
+		line = strings.TrimSpace(line)
+
+		//skip blank line
+		if len(line) != 0 {
+
+			if strings.Contains(line, "[code]") {
+				hasReachedCodeSection = true
+				continue
+			}
+
+			if hasReachedCodeSection {
+				if !strings.Contains(line, "#") &&
+					!strings.Contains(line, "[") &&
+					!strings.Contains(line, "]") &&
+					!strings.Contains(line, "=") &&
+					!strings.Contains(line, ";") {
+				}
+			} else {
+				//build string for ini parsing
+				if strings.Contains(line, "[") ||
+					strings.Contains(line, "]") ||
+					strings.Contains(line, "=") {
+
+					strForIniParsing = strForIniParsing + line + "\n"
+				}
+			}
 		}
 
 		if err != nil {
@@ -351,6 +487,81 @@ func main() {
 		}
 	}
 
-	fmt.Println("If any error appears, cmdfile is not completed. Press ENTER to exit")
-	fmt.Scanln()
+	file.Close()
+
+	return strForIniParsing
+}
+
+func parseCode(filename string) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		printError(err)
+		log.Fatal(err)
+	}
+
+	var line string
+	scanner := bufio.NewScanner(file)
+
+	hasReachedCodeSection := false
+
+	for scanner.Scan() {
+
+		line = scanner.Text()
+
+		line = strings.TrimSpace(line)
+
+		//skip blank line
+		if len(line) != 0 {
+
+			if strings.Contains(line, "[code]") {
+				hasReachedCodeSection = true
+				continue
+			}
+
+			if hasReachedCodeSection {
+				if !strings.Contains(line, "#") &&
+					!strings.Contains(line, "[") &&
+					!strings.Contains(line, "]") &&
+					!strings.Contains(line, "=") &&
+					!strings.Contains(line, ";") {
+					err = processCmd(line)
+				}
+			}
+		}
+
+		if err != nil {
+			break
+			log.Fatal(err)
+		}
+	}
+
+	file.Close()
+}
+
+func main() {
+
+	filename := os.Args[1]
+
+	if len(os.Args) != 2 {
+		printError(errors.New("Please specify a cmdfile"))
+		os.Exit(1)
+	}
+
+	str := parseIniSections(filename)
+
+	//convert string to io.Reader
+	input := bytes.NewBufferString(str)
+
+	var err error
+
+	cmdfile, err = ini.Load(input)
+	if err != nil {
+		printError(err)
+	}
+
+	parseCode(filename)
+
+	fmt.Println("")
+	fmt.Println("If any error appears, cmdfile is not completed")
 }
